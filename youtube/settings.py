0ce75ef8402ab11d04eee0f9981d36a5e490b4f0
sourmundada <sourmundada@gmail.com>
2021-04-26 13:10:20 +0530

    deployment

--- youtube/settings.py
+++ youtube/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = '9-*c#q=ywrw#rl&vi1k!4#%91pggvmrybx)wk#!j4uuf!@epjm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
